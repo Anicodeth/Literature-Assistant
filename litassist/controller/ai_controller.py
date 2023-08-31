58155ae5f12790f171f6da9cf91da66e4c2e6bc5
Ananya <afmtrendaid@gmail.com>
2023-08-31 09:50:36 -0700

    More endpoints

--- litassist/controller/ai_controller.py
+++ litassist/controller/ai_controller.py
@@ -10,3 +10,35 @@ def get_poem(prompt:str):
 @router.get('/story/{prompt}')
 def get_story(prompt:str):
     return lit_service.get_story(prompt)
+
+@router.get('/essay/{prompt}')
+def get_essay(prompt:str):
+    return lit_service.get_essay(prompt)
+
+@router.get('/fiction/{prompt}')
+def get_fiction(prompt:str):
+    return lit_service.get_fiction(prompt)
+
+@router.get('/lyrics/{prompt}')
+def get_lyric(prompt:str):
+    return lit_service.get_lyrics(prompt)
+
+@router.get('/dialogue/{prompt}')
+def get_dialogue(prompt:str):
+    return lit_service.get_dialogue(prompt)
+
+
+
+@router.get('/nonfiction/{prompt}')
+def get_nonfiction(prompt:str):
+    return lit_service.get_nonfiction(prompt)
+
+@router.get('/novel/{prompt}')
+def get_novel(prompt:str):
+    return lit_service.get_novel(prompt)
+
+@router.get('/script/{prompt}')
+def get_script(prompt:str):
+    return lit_service.get_script(prompt)
+
+
--- litassist/services/ai_services/literature_service.py
+++ litassist/services/ai_services/literature_service.py
@@ -9,4 +9,33 @@ class LitService:
         prompt = f"Write s story about, '{prompt}'."
         result = generate_text(prompt)
         return result
+    def get_essay(self, prompt):
+        prompt = f"Write an essay about, '{prompt}'."
+        result = generate_text(prompt)
+        return result
+    def get_lyrics(self, prompt):
+        prompt = f"Write lyrics about, '{prompt}'."
+        result = generate_text(prompt)
+        return result
+    def get_script(self, prompt):
+        prompt = f"Write a script about, '{prompt}'."
+        result = generate_text(prompt)
+        return result
+    def get_dialogue(self, prompt):
+        prompt = f"Write a dialogue about, '{prompt}'."
+        result = generate_text(prompt)
+        return result
+    def get_novel(self, prompt):
+        prompt = f"Write a novel about, '{prompt}'."
+        result = generate_text(prompt)
+        return result
+    def get_fiction(self, prompt):
+        prompt = f"Write a fiction about, '{prompt}'."
+        result = generate_text(prompt)
+        return result
+    def get_nonfiction(self, prompt):
+        prompt = f"Write a nonfiction about, '{prompt}'."
+        result = generate_text(prompt)
+        return result
+    
     
